4f2cb2d2f844093118857447a62da81f9661d0e5
emilopez <emiliano.lopez@gmail.com>
2023-03-31 14:44:48 -0300

    version v1.0b

--- webapp.py
+++ webapp.py
@@ -17,7 +17,9 @@ st.markdown(""" <style>
 footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
-dev_column_names = {'v1.0': ['datetime', 'bateria', 'cmca']}
+dev_column_names = {'v1.0a': ['datetime', 'bateria', 'cmca'],
+                    'v1.0b': ['datetime', 'bateria', 'nf', 'cmca'],
+                    }
     
 
 st.header("DataViz - Visualizador de datos")
@@ -31,18 +33,18 @@ with principal:
 
     uploaded_files = c1.file_uploader("Cargar archivos CSV", type=["csv"], accept_multiple_files=True)
     # version del dispositivo
-    dev_version = c2.selectbox('Modelo dispositivo',('v1.0', 'v0.99'))
-    if dev_version == "v1.0":
+    dev_version = c2.selectbox('Modelo dispositivo',('v1.0a', 'v1.0b'))
+    if dev_version == "v1.0a" or dev_version == "v1.0b":
         column_names = dev_column_names[dev_version]
         column_datetime = column_names[0]
-        c3.table(pd.DataFrame(column_names,columns=["Columnas"]))
+        c3.table(pd.DataFrame(column_names, columns=["Columnas"]))
         if uploaded_files:
             dfs = (pd.read_csv(f, sep=";", parse_dates=[column_datetime], names=column_names, comment='#') for f in uploaded_files)
             eq = pd.concat(dfs, ignore_index=True)
             column_plot = st.columns(2)
             for i,column in enumerate(column_names[1:]):
                 fig = go.Figure()
-                fig.add_trace(go.Scattergl(x=eq[column_datetime], y=eq[column], name=column))
+                fig.add_trace(go.Scattergl(x=eq[column_datetime], y=eq[column], name=column, mode="lines+markers"))
                 fig.update_layout(title=column.upper(),xaxis_title=column_datetime)
                 column_plot[(i+1)%2].plotly_chart(fig, use_container_width=True)
     else:
